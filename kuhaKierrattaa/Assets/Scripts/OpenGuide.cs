ac936a6dd7e9889d498a6191d58cd62b8f96065a
Teo-n <teo.nikulainen@edu.turkuamk.fi>
2020-03-25 16:55:08 +0200

    Added guide scene, return to menu button and instructions

---
+++ kuhaKierrattaa/Assets/Scripts/OpenGuide.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenGuide : MonoBehaviour
+{
+    public void GotoGuideScene()
+    {
+        SceneManager.LoadScene("GuideScene");
+    }
+}
+
