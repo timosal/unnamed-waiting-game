7f0b960e1c8c45dabac9ec086c30e7efa50455b4
timosal <44462804+timosal@users.noreply.github.com>
2020-03-29 21:46:12 +0300

    Added GameTimer
    Added working GameTimer, when timer reaches 0 game goes to the EndScene

--- kuhaKierrattaa/Assets/Scripts/GameStart.cs
+++ kuhaKierrattaa/Assets/Scripts/GameStart.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class GameStart : MonoBehaviour
 {
     public void GotoGameScene()
---
+++ kuhaKierrattaa/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameTimer : MonoBehaviour
+{
+    float nytAika = 0f; //Paljonko aikaa jäljellä
+    float aloitusAika = 30f;  //Paljonko aikaa alussa
+
+    [SerializeField] Text TextTimer;
+    void Start()
+    {
+        nytAika = aloitusAika;  //asetetaan jäljellä olevaksi ajaksi haluttu määrä
+    }
+
+    void Update()
+    {
+        nytAika -= 1 * Time.deltaTime;  //Joka sekunti vähennetään  sen hetkisestä ajasta yksi
+        TextTimer.text = nytAika.ToString("0"); //Asetetaan teksti objektiin aika mikä muutetaan string muotoon
+
+        if(nytAika <= 0)  //Kun aika menisi 0 alapuolella niin asetetaan aika 0:ksi, kun aika 0 niin peli siirtyy loppuruutuun.
+        {
+            nytAika = 0; 
+            SceneManager.LoadScene("EndScene");  
+        }
+    }
+}
